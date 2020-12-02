3cfc398fb19e4e16a436effc5131eb8e5de7042b
Nick <devwasik@gmail.com>
2020-12-01 19:44:08 -0600

    initial

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import React, { useState } from 'react'
+
+function Home() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/utils/list-context.js
@@ -0,0 +1,57 @@
+import React, { useReducer, createContext } from "react";
+
+export const ListContext = createContext();
+
+const initialState = {
+  items: [
+    {
+      id: 1,
+      task: "Go to store",
+    },
+    {
+      id: 2,
+      task: "Laundry",
+    },
+    {
+      id: 3,
+      task: "Relax"
+    },
+  ],
+  loading: false,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return {
+        items: [...state.items, action.payload],
+      };
+    case "DEL_ITEM":
+      return {
+        items: state.items.filter(
+          (item) => item.id !== action.payload
+        ),
+      };
+    case "START":
+      return {
+        loading: true,
+      };
+    case "COMPLETE":
+      return {
+        loading: false,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+export const ListContextProvider = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <ListContext.Provider value={[state, dispatch]}>
+      {props.children}
+    </ListContext.Provider>
+  );
+};
